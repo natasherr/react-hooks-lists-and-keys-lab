c8b66fcfca41f1e2c1442e156595b06481c79751
Ashley Natasha <ashleyotsiula@gmail.com>
2024-10-30 22:32:04 +0300

    lists

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,16 @@
 import React from "react";
+import ProjectList from "./ProjectList";
 
 function ProjectItem({ name, about, technologies }) {
+  const items = technologies.map((technology) => (
+    <span key={technology}>{technology} </span>
+  ))
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {items}
       </div>
     </div>
   );
